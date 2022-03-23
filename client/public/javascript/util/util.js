c565284cf19b79463a29dc27756fc90f94b92661
sju02048 <sju02048@naver.com>
2022-03-23 15:45:15 +0900

    Feat: Util 함수 추가
    $, $$, fetchData

--- client/public/javascript/util/util.js
+++ client/public/javascript/util/util.js
@@ -1,5 +1,35 @@
-export const $ = (selector, base = document.body) =>
-  base.querySelector(selector);
+export const $ = (selector, base = document.body) => {
+  let node;
+  try {
+    node = base.querySelector(selector);
+  } catch (error) {
+    console.log(error);
+  }
+  return node;
+};
+
+export const $$ = (selectors, base = document) => {
+  let nodes;
+  try {
+    nodes = base.querySelectorAll(selectors);
+  } catch (error) {
+    console.log(error);
+  }
+  return nodes;
+};
 
 export const delay = (ms) =>
   new Promise((resolve) => setTimeout(() => resolve(ms), ms));
+
+export const fetchData = async (url) => {
+  try {
+    const data = await fetch(url);
+    if (data.ok) {
+      return data.json();
+    } else {
+      throw new Error(data.statusText);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
